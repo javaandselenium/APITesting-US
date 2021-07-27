7272ad3224f40e85031d546f68cf6ebe499b7cf4
QSP <QSP@QSP>
2021-07-27 08:22:24 +0530

    Hi

---
+++ src/test/java/com/TestYantra/USAP1Project/scripts/Test1.java
@@ -0,0 +1,11 @@
+package com.TestYantra.USAP1Project.scripts;
+
+import org.testng.annotations.Test;
+
+public class Test1 {
+@Test
+public void demo() {
+	System.out.println("Hello good morning");
+}
+	
+}
